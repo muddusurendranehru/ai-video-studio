c5c640beb57fd2208e3aa3487d912434115f129c
Dr Nehru Healthcare System <drnehru@homahealthcare.com>
2025-08-21 11:31:07 +0530

    Initial commit: AI Video Studio

---
+++ ai-video-studio/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import VideoGenerationApp from './components/VideoGenerationApp';
+
+function App() {
+  return <VideoGenerationApp />;
+}
+
+export default App;
---
+++ ai-video-studio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import VideoGenerationApp from './components/VideoGenerationApp';
+import './App.css';
+
+function App() {
+  return (
+    <VideoGenerationApp />
+  );
+}
+
+export default App;
